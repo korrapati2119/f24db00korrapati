d2ef515c929fcd94319845ab361e762ecd295495
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-14 15:32:39 -0600

    description

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const vehicle_controllers = require('../controllers/vehicles');
+const vehicles_controllers = require('../controllers/vehicles');
 
 // Vehicle Routes
-router.get('/', vehicle_controllers.getAllDocuments); // List all vehicles
-router.post('/', vehicle_controllers.vehicle_create_post); // Create a new vehicle
-router.get('/:id', vehicle_controllers.vehicle_detail); // Get vehicle by ID
-router.put('/:id', vehicle_controllers.vehicle_update_put); // Update vehicle by ID
-router.delete('/:id', vehicle_controllers.vehicle_delete); // Delete vehicle by ID
+router.get('/', vehicles_controllers.getAllDocuments); // List all vehicles
+router.post('/', vehicles_controllers.vehicle_create_post); // Create a new vehicle
+router.get('/:id', vehicles_controllers.vehicle_detail); // Get vehicle by ID
+router.put('/:id', vehicles_controllers.vehicle_update_put); // Update vehicle by ID
+router.delete('/:id', vehicles_controllers.vehicle_delete); // Delete vehicle by ID
 
 module.exports = router;
