e25c2867b0f6d43b2a7b8e293592959b983e2999
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-14 15:15:49 -0600

    description

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+
 const connectionString = process.env.MONGO_CON;
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
+db.once('open', function () {
   console.log("Connection to DB succeeded");
 });
+
 var app = express();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const gridRouter = require('./routes/grid');
-const pickRouter = require('./routes/pick');
+var gridRouter = require('./routes/grid');
+const pickRouter = require('./routes/pick');  // <-- Add this line
 const resourceRouter = require('./routes/resource');
-var vehicleRouter = require('./routes/vehicle');  
+var vehiclesRouter = require('./routes/vehicles');  
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,40 +33,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/vehicle', vehicleRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/grid', gridRouter);
-//app.use('/selector', pickRouter);
-app.use('/resource', resourceRouter);  // Route for all resource-related requests
+app.use('/selector', pickRouter);  // Using pickRouter now that it's defined
+app.use('/resource', resourceRouter);
+app.use('/vehicles', vehiclesRouter);
 
-//Error handler
+// Error handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {}; // Show detailed error in dev environment
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
-  res.render('error'); // Render error page
-});
-
-// Static Vehicle Route Example
-app.get('/vehicle', (req, res) => {
-  const results = [
-    { vehicle_name: 'Sedan', vehicle_type: 'Car', max_speed: 180 },
-    { vehicle_name: 'Sport Bike', vehicle_type: 'Motorcycle', max_speed: 220},
-    { vehicle_name: 'SUV', vehicle_type: 'Car', max_speed: 160}
-  ];
-  res.render('vehicle', { results }); // Pass results to Pug
-});
-// Vehicles Route - Fetch from Database
-app.get('/resource/vehicle', async (req, res) => {
-  try {
-    // Fetch all vehicles from the database
-    const vehicle = await Vehicle.find();
-    res.json(vehicle); // Send vehicles as JSON
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Failed to fetch vehicles" });
-  }
+  res.render('error');
 });
 
 module.exports = app;
