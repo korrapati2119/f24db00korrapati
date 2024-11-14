7679536cb2d36b5f91690282c13e9499a3423704
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-14 15:41:33 -0600

    description

--- routes/resource.js
+++ routes/resource.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 const api_controller = require('../controllers/api');
-const vehicles_controller = require('../controllers/vehicles'); // Make sure the path is correct
+const vehicle_controller = require('../controllers/vehicles'); // Make sure the path is correct
 
 // API Route - Overview of supported routes
 router.get('/', api_controller.api);
+
+router.get('/vehicles', vehicle_controller.getAllDocuments);
 
 // Vehicle Routes
 router.get('/resource/vehicles', vehicles_controller.getAllDocuments);  // List all vehicles
