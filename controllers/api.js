89357f3ba97cbc3dd7deac31fd09ae2fc6534cbd
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-13 23:03:39 -0600

    controller

--- controllers/api.js
+++ controllers/api.js
@@ -1,7 +1,7 @@
 // API for our resources
 exports.api = function(req, res) {
     res.write('[');
-    res.write('{"resource":"sculptures", ');
+    res.write('{"resource":"vehicles", ');
     res.write(' "verbs":["GET","POST","PUT","DELETE"] ');
     res.write('}');
     res.write(']');
