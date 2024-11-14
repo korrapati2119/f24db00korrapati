ae84ae5a2f868528f49b602329b3afce8943ccd9
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-14 17:47:06 -0600

    description

--- routes/resource.js
+++ routes/resource.js
@@ -1,19 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-const api_controller = require('../controllers/api');
-const vehicleController = require('../controllers/vehicles'); // Make sure the path is correct
+// Correct path to vehicle controller
+const vehicle_controller = require('../controllers/vehicles'); 
 
-// API Route - Overview of supported routes
-router.get('/', api_controller.api);
-
-router.get('/vehicles', vehicle_controller.getAllDocuments);
-
-// Vehicle Routes
-router.get('/resource/vehicles', vehicle_controller.getAllDocuments);  // List all vehicles
-router.post('/vehicles', vehicleController.vehicle_create_post); // Create a new vehicle
-router.get('/resource/vehicles/:id', vehicle_controller.vehicle_detail); // View a single vehicle by ID
-router.put('/resource/vehicles/:id', vehicle_controller.vehicle_update_put); // Update a vehicle by ID
-router.delete('/resource/vehicles/:id', vehicle_controller.vehicle_delete); // Delete a vehicle by ID
+// Define routes
+router.get('/vehicles', vehicle_controller.getAllDocuments);  // Get all vehicles
+router.post('/vehicles', vehicle_controller.vehicle_create_post);  // Create new vehicle
+router.get('/vehicles/:id', vehicle_controller.vehicle_detail);  // Get vehicle by ID
+router.put('/vehicles/:id', vehicle_controller.vehicle_update_put);  // Update vehicle
+router.delete('/vehicles/:id', vehicle_controller.vehicle_delete);  // Delete vehicle
 
 module.exports = router;
