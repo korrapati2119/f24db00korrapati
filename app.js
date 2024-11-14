8e809b410c56d449553149eea4a357276df88a36
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-14 11:39:28 -0600

    descriptions

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const connectionString = process.env.MONGO_CON;
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function () {
+  console.log("Connection to DB succeeded");
+});
+var app = express();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const gridRouter = require('./routes/grid');
 const pickRouter = require('./routes/pick');
 const Vehicle = require('./models/vehicles');
+const resourceRouter = require('./routes/resource');
+var vehiclesRouter = require('./routes/vehicles');  
 
-var app = express();
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,49 +33,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set up routes
+app.use('/vehicles', vehiclesRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/grid', gridRouter);
 app.use('/selector', pickRouter);
-
-var resourceRouter = require('./routes/resource'); // Ensure this path is correct
-
 app.use('/resource', resourceRouter);  // Route for all resource-related requests
 
-// Static Vehicle Route Example
-app.get('/vehicles', (req, res) => {
-  const results = [
-    { vehicles_name: 'Sedan', vehicle_type: 'Car', max_speed: 180 },
-    { vehicles_name: 'Sport Bike', vehicle_type: 'Motorcycle', max_speed: 220},
-    { vehicles_name: 'SUV', vehicle_type: 'Car', max_speed: 160}
-  ];
-  res.render('vehicles', { results }); // Pass results to Pug
-});
-
-// Resource Route
-app.get('/resource', (req, res) => {
-  res.send('Resource page');
-});
-
-// Vehicles Route - Fetch from Database
-app.get('/resource/vehicles', async (req, res) => {
-  try {
-    // Fetch all vehicles from the database
-    const vehicles = await Vehicle.find();
-    res.json(vehicles); // Send vehicles as JSON
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Failed to fetch vehicles" });
-  }
-});
-
-// General Error Handling Route (for unknown routes)
-app.use(function(req, res, next) {
-  next(createError(404)); // Trigger 404 error if route doesn't match
-});
-
-// Error handler
+//Error handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {}; // Show detailed error in dev environment
@@ -72,16 +48,15 @@ app.use(function(err, req, res, next) {
   res.render('error'); // Render error page
 });
 
-// MongoDB connection setup
-require('dotenv').config();
-const mongoose = require('mongoose');
-const connectionString = process.env.MONGO_CON;
-
-mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', function () {
-  console.log("Connection to DB succeeded");
-});
-
-module.exports = app;
+// List of all Vehicles
+exports.costume_list = async function(req, res) {
+  try{
+  theVehicls = await Vehicle.find();
+  res.send(theVehicles);
+  }
+  catch(err){
+  res.status(500);
+  res.send(`{"error": ${err}}`);
+  } 
+ };
+ 
--- routes/resource.js
+++ routes/resource.js
@@ -6,6 +6,9 @@ module.exports = router;
 var api_controller = require('../controllers/api');
 var vehicle_controller = require('../controllers/vehicles');
 
+// Example import
+const { getAllDocuments } = require('../controllers/vehicles'); // Make sure the path is correct
+
 // API Route
 router.get('/',api_controller.api);
 
@@ -15,5 +18,6 @@ router.post('/vehicles', vehicle_controller.vehicle_create_post); // Create new 
 router.get('/vehicles/:id', vehicle_controller.vehicle_detail); // View single vehicle
 router.put('/vehicles/:id', vehicle_controller.vehicle_update_put); // Update vehicle
 router.delete('/vehicles/:id',vehicle_controller.vehicle_delete); // Delete vehicle
+router.get('/resource/vehicles', getAllDocuments);
 
 module.exports = router;
