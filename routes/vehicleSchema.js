cd8d76bcee667107692f29e00d4fa9b22f37c7a9
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-14 12:12:45 -0600

    descriptions

--- routes/vehicleSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-const vehicleSchema = mongoose.Schema({
-costume_type: String,
-size: String,
-cost: Number
-})
-module.exports = mongoose.model("vehicle", 
-vehicleSchema)
