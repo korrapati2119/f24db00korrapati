8ff041daeaf5f397d4e98f21d669dc51e2695034
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-13 23:49:56 -0600

    vehicles

--- controllers/vehicles.js
+++ controllers/vehicles.js
@@ -4,7 +4,7 @@ const Vehicle = require('../models/vehicles');
 // List all vehicles
 exports.vehicle_list = async (req, res) => {
   try {
-    const vehicles = await Vehicle.find();
+    const vehicles = await Vehicles.find();
     res.status(200).json(vehicles);
   } catch (err) {
     res.status(500).json({ message: 'Failed to fetch vehicles' });
