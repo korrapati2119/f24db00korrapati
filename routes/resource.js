ea9501f5e5f5bfa2860b491d760e8c5b9ac56324
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-14 15:42:25 -0600

    description

--- routes/resource.js
+++ routes/resource.js
@@ -10,10 +10,10 @@ router.get('/', api_controller.api);
 router.get('/vehicles', vehicle_controller.getAllDocuments);
 
 // Vehicle Routes
-router.get('/resource/vehicles', vehicles_controller.getAllDocuments);  // List all vehicles
-router.post('/resource/vehicles', vehicles_controller.vehicle_create_post); // Create a new vehicle
-router.get('/resource/vehicles/:id', vehicles_controller.vehicle_detail); // View a single vehicle by ID
-router.put('/resource/vehicles/:id', vehicles_controller.vehicle_update_put); // Update a vehicle by ID
-router.delete('/resource/vehicles/:id', vehicles_controller.vehicle_delete); // Delete a vehicle by ID
+router.get('/resource/vehicles', vehicle_controller.getAllDocuments);  // List all vehicles
+router.post('/resource/vehicles', vehicle_controller.vehicle_create_post); // Create a new vehicle
+router.get('/resource/vehicles/:id', vehicle_controller.vehicle_detail); // View a single vehicle by ID
+router.put('/resource/vehicles/:id', vehicle_controller.vehicle_update_put); // Update a vehicle by ID
+router.delete('/resource/vehicles/:id', vehicle_controller.vehicle_delete); // Delete a vehicle by ID
 
 module.exports = router;
