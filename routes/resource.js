48464fd776b9968fb39313f435bd4f7e109a2456
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-15 09:58:59 -0600

    description

--- routes/resource.js
+++ routes/resource.js
@@ -8,7 +8,6 @@ const vehicle_controller = require('../controllers/vehicles');
 router.get('/vehicles', vehicle_controller.getAllDocuments);  // Get all vehicles
 router.post('/vehicles', vehicle_controller.vehicle_create_post);  // Create new vehicle
 router.get('/vehicles/:id', vehicle_controller.vehicle_detail);  // Get vehicle by ID
-router.put('/resource/vehicles/:id', vehicleController.vehicle_update_put);
 router.put('/resource/vehicles/:id', vehicle_controller.vehicle_update_put); // Update vehicle
 router.delete('/vehicles/:id', vehicle_controller.vehicle_delete);  // Delete vehicle
 // GET request for one vehicle.
