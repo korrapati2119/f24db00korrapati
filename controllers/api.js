ce8be9df5a147f25770ad5c60ab52ebdc5549383
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-21 11:44:28 -0600

    deleting vehicle

--- controllers/api.js
+++ controllers/api.js
@@ -1,9 +1,9 @@
-exports.api = (req, res) => {
+exports.api = function (req, res) {
   res.write('API for Vehicle Management\n');
   res.write('GET /resource/vehicles - List all vehicles\n');
   res.write('POST /resource/vehicles - Create a new vehicle\n');
   res.write('GET /resource/vehicles/:id - Get a vehicle by ID\n');
   res.write('PUT /resource/vehicles/:id - Update a vehicle by ID\n');
   res.write('DELETE /resource/vehicles/:id - Delete a vehicle by ID\n');
-  res.end();
+  res.send();
 };
