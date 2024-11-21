c26abf0c8cf4f7ca059df7ec96e486a79e69559d
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-20 19:07:05 -0600

    deleting vehicle

--- app.js
+++ app.js
@@ -47,5 +47,8 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error'); // Render error page
 });
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 module.exports = app;
