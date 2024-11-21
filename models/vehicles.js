daf372b01570e63710a5cfff830d15a9eb1ad14f
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-20 22:02:18 -0600

    deleting vehicle

--- models/vehicles.js
+++ models/vehicles.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 
-// Define the vehicle schema
 const vehicleSchema = new mongoose.Schema({
   vehicle_name: { type: String, required: true },
   price: { type: Number, required: true },
-  functionality: { type: String, required: true },
+  functionality: { type: String, required: true }
 });
 
-// Create the vehicle model based on the schema
-const Vehicle = mongoose.model('Vehicle', vehicleSchema);
-
-// Export the vehicle model
-module.exports = Vehicle;
+module.exports = mongoose.model('Vehicles', vehicleSchema);
