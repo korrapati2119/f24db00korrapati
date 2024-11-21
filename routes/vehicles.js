535584fc35511c60346cc4dd09a680890a5ea538
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-21 14:37:06 -0600

    deleting vehicle

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const vehicleController = require('../controllers/vehicles'); // Import the vehicle controller
 
 // Routes for vehicle CRUD operations
-router.post('/', vehicleController.vehicle_create_post);  // Create a new vehicle
+router.post('/', vehicleController.vehicles_create_post);  // Create a new vehicle
 router.get('/', vehicleController.getAllDocuments);      // Get all vehicles
-router.get('/:id', vehicleController.vehicle_detail);    // Get details of a specific vehicle by ID
-router.put('/:id', vehicleController.vehicle_update_put); // Update a specific vehicle by ID
-router.delete('/:id', vehicleController.vehicle_delete);  // Delete a specific vehicle by ID
+router.get('/:id', vehicleController.vehicles_detail);    // Get details of a specific vehicle by ID
+router.put('/:id', vehicleController.vehicles_update_put); // Update a specific vehicle by ID
+router.delete('/:id', vehicleController.vehicles_delete);  // Delete a specific vehicle by ID
 
 // Route to render a view for a single vehicle
 router.get('/view/:id', vehicleController.vehicles_view_one_Page);
