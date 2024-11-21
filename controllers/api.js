9f0c88997491f764e6032fe8e24bad3d10e29a3e
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-21 11:46:25 -0600

    deleting vehicle

--- controllers/api.js
+++ controllers/api.js
@@ -1,9 +1,8 @@
-exports.api = function (req, res) {
-  res.write('API for Vehicle Management\n');
-  res.write('GET /resource/vehicles - List all vehicles\n');
-  res.write('POST /resource/vehicles - Create a new vehicle\n');
-  res.write('GET /resource/vehicles/:id - Get a vehicle by ID\n');
-  res.write('PUT /resource/vehicles/:id - Update a vehicle by ID\n');
-  res.write('DELETE /resource/vehicles/:id - Delete a vehicle by ID\n');
+exports.api = function(req, res) {
+  res.write('[');
+  res.write('{"resource":"heritageSites", ');
+  res.write(' "verbs":["GET","PUT", "DELETE"] ');
+  res.write('}');
+  res.write(']');
   res.send();
 };
