b3ae3d9cd55f94d7f01ff03299ea22ebfabe47cc
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-23 21:35:20 -0600

    adding

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+
 const Vehicle = require('../models/vehicles');
 const router = express.Router();
 
@@ -12,16 +12,18 @@ console.log('vehicle_list:', vehiclesController.vehicle_list);
 router.get('/', vehiclesController.vehicle_list); // List all vehicles
 router.post('/create', vehiclesController.vehicle_create_post); // Create a new vehicle
 router.post('/', vehiclesController.vehicle_create);
-router.get('/:id', vehiclesController.vehicle_detail); // Get vehicle details by ID
 router.put('/vehicles/:id', vehiclesController.vehicle_update_put); // Update a vehicle
 router.delete('/:id', vehiclesController.vehicle_delete); // Delete a vehicle
 
 // View Routes
 router.get('/create/page', vehiclesController.vehicle_create_Page); // Render Create Page
 router.get('/update', vehiclesController.vehicle_update_Page); // Render Update Page
+router.get('/:id', vehiclesController.vehicle_detail); // Get vehicle details by ID
+
 router.get('/delete/page', vehiclesController.vehicle_delete_Page); // Render Delete Page
 router.get('/view/page', vehiclesController.vehicle_view_all_Page); // Render All Vehicles Page
 router.get('/view/:id', vehiclesController.vehicle_view_one_Page); // Render Single Vehicle Page
+
 
 router.post('/update', async (req, res) => {
     try {
